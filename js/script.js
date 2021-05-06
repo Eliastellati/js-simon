e2fbdce69dcb72c4de375df191056b6def7e3291
eliastellati <eliastellatibvb@gmail.com>
2021-05-06 17:02:39 +0200

    funziona in console

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
 var array= [];
-var rightNum =[];
-score= 0;
+var attempts= [];
+var rightNum = [];
 
-for (i = 0; i < 5; i++) {
-    numero = getRndInteger(1, 100);
-    array.push(numero);
-}
+while (array.length < 5){
+    var nuovoNum = Math.floor(Math.random()*100) + 1;
+    if(!cercaElemento(nuovoNum, array)){
+      array.push(nuovoNum)
+    }
+  }
 
-console.log(array[i]);
+console.log(array);
 //come fare alert che espone i numeri?
 
 alert(array);
 
 //Timer 30 secondi?
 
-var timer= setTimeout (userGuess, 3000);
-
-console.log(array);
-
-// verificare quanti numeri sono stati azzeccati
-
-console.log(rightNum);
-
-
-
-
-
-
-function simonNum() {
-    alert(array[i]);
-}
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-function userGuess(num) {
-    for (i = 0 ; i<array.length; i++) {
-        guess= parseInt(prompt('inserisci i numeri uno alla volta'));
-        if (guess == array[i]) {
-            rightNum.push(guess);
+setTimeout(function(){
+    while (attempts.length < 5) {
+        var guess= parseInt(prompt('inserisci un numero che ricordi'));
+        if (!cercaElemento(guess, attempts) && !isNaN(guess)) {
+            attempts.push(guess);
         }
+        console.log(attempts);
+        controllo(array, attempts);
     }
+}, 3000);
+
+document.getElementById('result').innerHTML= 'hai indovinato ' + rightNum.length;
+
+//Functions
+function cercaElemento(elem, array){
+    for (var i = 0; i < array.length; i++) {
+      if (elem == array[i]) {
+        return true;
+       }
+    }
+    return false;
+  
+}
+
+function controllo(numeri, tentativi){
+    // confronto i due array e restituisco la quantità di numeri indovinati e quali sono.
+  
+    for (var i = 0; i < 5; i++) {
+      if (cercaElemento(attempts[i], array)){
+        rightNum.push(attempts[i]);
+      }
+    }
+  
+    console.log("Hai indovinato " + rightNum.length + " numeri.");
+    console.log("I numeri indovinati sono: " + rightNum);
+  
 }
 
